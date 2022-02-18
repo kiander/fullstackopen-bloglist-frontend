a1a5bf00d98d2d6f1356a4d5549aa0603eb6dfb6
Otto Kiander <otto.kiander@tuni.fi>
2022-02-18 12:45:50 +0200

    add blog visibilty

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -65,35 +66,16 @@ const App = () => {
     window.location.reload();
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-          <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>      
-  )
-
   return (
   <div>
-  <h2>Login</h2>
       {user === null ?
-      loginForm() :
+      <LoginForm
+        username={username}
+        password={password}
+        handleUsernameChange={({ target }) => setUsername(target.value)}
+        handlePasswordChange={({ target }) => setPassword(target.value)}
+        handleSubmit={handleLogin}
+      /> :
       <div>
         <p>{user.name} logged in</p>
         <BlogForm createBlog={addBlog}/><br /> 
@@ -101,7 +83,7 @@ const App = () => {
       </div>
     }
     <div>
-      <h2>blogs</h2>
+      <h2>Blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 
 const BlogForm = ({ createBlog }) => {
+  const [addBlogVisible, setAddBlogVisible] = useState(false)
   const [title, setTitle] = useState('') 
   const [author, setAuthor] = useState('')
   const [url, setUrl] = useState('')
+
+  const hideWhenVisible = { display: addBlogVisible ? 'none' : '' }
+  const showWhenVisible = { display: addBlogVisible ? '' : 'none' }
 
   const addBlog = (event) => {
     event.preventDefault()
@@ -15,28 +19,35 @@ const BlogForm = ({ createBlog }) => {
   }
 
   return (
-    <form onSubmit={addBlog}>
-    <div> Title 
-      <input
-        value={title}
-        onChange={({ target }) => setTitle(target.value)}
-      />
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setAddBlogVisible(true)}>Add Blog</button>
+      </div>
+      <div style={showWhenVisible}>
+        <form onSubmit={addBlog}>
+        <div> Title 
+          <input
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div> Author 
+          <input
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div> Url 
+          <input
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <br /> 
+        <button type="submit">save</button>
+        </form>
+      </div>
     </div>
-    <div> Author 
-      <input
-        value={author}
-        onChange={({ target }) => setAuthor(target.value)}
-      />
-    </div>
-    <div> Url 
-      <input
-        value={url}
-        onChange={({ target }) => setUrl(target.value)}
-      />
-    </div>
-    <br /> 
-    <button type="submit">save</button>
-  </form>  
   )
 }
 
